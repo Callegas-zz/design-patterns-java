e47d95d9ee9ad276420e34a8e6e3fc5f1fbd9eab
callegas <callegas.f@gmail.com>
2017-09-15 10:24:21 -0300

    create loader.

--- creational/singleton/singleton-enum-quotes/src/main/java/com/callegasdev/ThoughtsEnum.java
+++ creational/singleton/singleton-enum-quotes/src/main/java/com/callegasdev/ThoughtsEnum.java
@@ -10,12 +10,15 @@ public enum ThoughtsEnum {
 
     Map<Integer, String> thoughtList = new HashMap<>();
 
-    public String randomThought(){
+    private void loadThoughts() {
         thoughtList.put(0, "Try not to become a man of success, but rather try to become a man of value.");
         thoughtList.put(1, "Great spirits have always encountered violent opposition from mediocre minds.");
         thoughtList.put(2, "Not everything that can be counted counts, and not everything that counts can be counted.");
         thoughtList.put(3, "Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.");
+    }
 
+    public String randomThought(){
+        loadThoughts();
         return thoughtList.get(new Random().nextInt(thoughtList.size()));
     }
 
